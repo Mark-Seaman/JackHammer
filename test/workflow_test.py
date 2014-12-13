8ea5a9d13b0d394d7ae0170d1ffc55cd74bf9dd1
Mark Seaman <Mark.B.Seaman@gmail.com>
2014-12-13 10:44:06 -0700

    Add workflow code

---
+++ test/workflow_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Build new python test script
+
+from os       import system
+
+
+def workflow_test():
+    system ('workflow')
+    
+if __name__ == "__main__":
+    workflow_test()
